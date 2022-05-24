23179e29f754951ac6367dabb8243e06cc684ed1
mani703 <mani703@naver.com>
2022-05-24 16:56:44 +0900

    dept router

---
+++ node02/src/routers/dept.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var mongo = require('mongodb');
+
+var router = express.Router();
+
+router.get('/list', function (req, res) {
+    var MongoClient = mongo.MongoClient;
+    var url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("testDB");
+        var coll = dbo.collection("emp");
+        var tot;
+        coll.find({}).toArray(function(err, result){
+            tot = result.length;
+        });
+        var cur = coll.find({}).skip(0).limit(5);
+        cur.toArray(function (err, result) {    // toArray(func) 배열로 변환
+            if (err) throw err;
+            res.render('dept/list', { total: tot, arr: result })
+            db.close();
+        });
+    });
+
+});
+
+module.exports = router;
