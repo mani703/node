72b934932651155d6c725cc9a285de8d805c5e90
mani703 <mani703@naver.com>
2022-05-24 17:49:42 +0900

    dept router

--- node02/src/routers/dept.js
+++ node02/src/routers/dept.js
@@ -12,10 +12,11 @@ router.get('/list', function (req, res) {
         var dbo = db.db("testDB");
         var coll = dbo.collection("emp");
         var tot;
-        coll.find({}).toArray(function(err, result){
+        coll.find({}).toArray(function (err, result) {
             tot = result.length;
         });
-        var cur = coll.find({}).skip(0).limit(5);
+        // var cur = coll.find({}).skip(0).limit(5);
+        var cur = coll.find({});
         cur.toArray(function (err, result) {    // toArray(func) 배열로 변환
             if (err) throw err;
             res.render('dept/list', { total: tot, arr: result })
@@ -24,5 +25,65 @@ router.get('/list', function (req, res) {
     });
 
 });
+
+router.get('/add', function (req, res) {
+    res.render('dept/add');
+})
+
+router.post('/add', function (req, res) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("testDB");
+        var myobj = { empno: req.body.empno, ename: req.body.ename, sal: req.body.sal };
+        dbo.collection("emp").insertOne(myobj, function (err, result) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+            res.redirect('/dept/list');
+        });
+    });
+
+});
+
+router.get('/row', function (req, res) {
+    var param = req.query._id;
+    var MongoClient = mongo.MongoClient;
+    var url = "mongodb://localhost:27017/";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("testDB");
+        dbo.collection("emp").findOne({ _id: new mongo.ObjectId(param) }, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.render('dept/emp', { bean: result });
+        });
+    });
+});
+
+router.post('/row', function (req, res) {
+    console.log(req.body);
+    var MongoClient = mongo.MongoClient;
+    var url = "mongodb://127.0.0.1:27017/";
+
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("testDB");
+        var myquery = { _id: new mongo.ObjectId(req.body._id) };
+        var newvalues = { $set: { empno: parseInt(req.body.empno), ename: req.body.ename } };
+        if (req.body.sal) newvalues['$set'].sal = parseInt(req.body.sal);
+        dbo.collection("emp").updateOne(myquery, newvalues, function (err, result) {
+            if (err) throw err;
+            console.log("updated", result);
+            db.close();
+        });
+    });
+    res.redirect("list");
+});
+
 
 module.exports = router;
