70cb05e6ad2db460f97a3124aa5998bb9eb73558
mani703 <mani703@naver.com>
2022-05-24 16:56:02 +0900

    node index settings

--- node02/src/index.js
+++ node02/src/index.js
@@ -3,13 +3,17 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
+var emp = require('./routers/emp');
+var dept = require('./routers/dept')
+
 app.use(express.static(path.resolve(__dirname, '../public')));
 app.set('views', path.resolve(__dirname, '../views'));
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/emp', require('./routers/emp'))
+app.use('/emp', emp);
+app.use('/dept', dept);
 
 app.listen(3000, function () {
   console.log('server starting...');
