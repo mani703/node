44dd4c7aebd22f983a618ddad7f3e315cb79e9b0
mani703 <mani703@naver.com>
2022-05-25 15:45:33 +0900

    node app settings

--- node03/app.js
+++ node03/app.js
@@ -19,7 +19,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 // routting...
 app.use('/', indexRouter);
